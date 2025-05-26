50e58ed79609b0c7b4e995dbc5299c1f5d443daa
helenewegener1 <helene.wegener@gmail.com>
2025-05-26 09:57:25 +0200

    presentation and solution 4A&4b

--- solutions/solution4B.R
+++ solutions/solution4B.R
@@ -6,7 +6,7 @@ make_boxplot <- function(df, plot_column){
     stop('The column to plot must be numcerial.')
   }
   
-  p <- ggplot(df, aes(y = .data[[plot_column]])) +
+  p <- ggplot(df, aes(y = !!sym(plot_column))) +
     geom_boxplot(fill = "#03579A") +
     labs(title = paste("Boxplot of", plot_column)) + 
     theme_bw()
